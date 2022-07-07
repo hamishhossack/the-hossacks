6e5d7d39b26d1d4319f53e21928a77938b748557
Hamish Hossack <hamishhossack@gmail.com>
2022-07-07 10:35:58 +0100

    ✨  all images with intersection observer

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,32 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"))
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function (
+      entries,
+      observer
+    ) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target
+          lazyImage.src = lazyImage.dataset.src
+          lazyImage.classList.remove("lazy")
+          lazyImageObserver.unobserve(lazyImage)
+        }
+      })
+    })
+
+    lazyImages.forEach(function (lazyImage) {
+      lazyImageObserver.observe(lazyImage)
+    })
+  } else {
+    // Possibly fall back to event handlers here
+  }
+})
+
 var main = (function ($) {
   var _ = {
     /**
